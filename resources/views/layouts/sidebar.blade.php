67d191a131750550e88a2681886a52a9589d0b08
George <cavidjaja@gmail.com>
2021-05-11 10:22:59 +0700

    no search

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -146,7 +146,7 @@
                         </button>
 
                         <!-- Topbar Search -->
-                        <form
+                        {{-- <form
                             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                             <div class="input-group">
                                 <input type="text" class="form-control bg-light border-0 small"
@@ -157,12 +157,12 @@
                                     </button>
                                 </div>
                             </div>
-                        </form>
+                        </form> --}}
 
                         <!-- Topbar Navbar -->
                         <ul class="navbar-nav ml-auto">
 
-                            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                            {{-- <!-- Nav Item - Search Dropdown (Visible Only XS) -->
                             <li class="nav-item dropdown no-arrow d-sm-none">
                                 <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -184,7 +184,7 @@
                                         </div>
                                     </form>
                                 </div>
-                            </li>
+                            </li> --}}
 
                             {{-- <!-- Nav Item - Alerts -->
                             <li class="nav-item dropdown no-arrow mx-1">
@@ -322,7 +322,7 @@
                                 <!-- Dropdown - User Information -->
                                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                     aria-labelledby="userDropdown">
-                                    <a class="dropdown-item" href="#">
+                                    {{-- <a class="dropdown-item" href="#">
                                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Profile
                                     </a>
@@ -333,8 +333,8 @@
                                     <a class="dropdown-item" href="#">
                                         <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Activity Log
-                                    </a>
-                                    <div class="dropdown-divider"></div>
+                                    </a> --}}
+                                    {{-- <div class="dropdown-divider"></div> --}}
                                     <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Logout
@@ -343,7 +343,6 @@
                             </li>
 
                         </ul>
-
                     </nav>
                     <!-- End of Topbar -->
                     <!-- Logout Modal-->
